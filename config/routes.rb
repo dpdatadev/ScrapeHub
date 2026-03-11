c7030fd43d3ef586d2693fc2f2c306c1ce76de8a
Derek <dpdatadev@gmail.com>
2026-03-11 05:49:02 -0500

    slight changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,11 @@ Rails.application.routes.draw do
   resources :command_configs
   resource :session
   resources :passwords, param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "/commands", to: "command#index"
   get "/commands/:id", to: "command#show"
   get "/configs", to: "command_configs#index"
-  # Defines the root path route ("/")
-  root "command#index"
+  get "/configs/new", to: "command_configs#new"
+  root "command_configs#index"
 end
